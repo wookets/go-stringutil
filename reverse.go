2c6fc5890c02df37a2c03d5fdb9e4ce3e6023bb5
Sean Wesenberg <wookets@gmail.com>
2018-03-31 22:49:55 -0500

    Rename to goutil

--- reverse.go
+++ reverse.go
@@ -1,4 +1,4 @@
-package stringutil
+package goutil
 
 // Reverse returns its argument string reversed rune-wise left to right.
 func Reverse(s string) string {
--- reverse_test.go
+++ reverse_test.go
@@ -1,4 +1,4 @@
-package stringutil
+package goutil
 
 import "testing"
 
